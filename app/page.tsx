38ab46ee2efda3e34875d46dd98bf68443d653a5
leneehe <qy.lena@gmail.com>
2024-10-03 18:44:11 -0400

    style with tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -12,14 +12,14 @@ export default function Home() {
   };
 
   return (
-    <div>
-      <div>
-        <h1>Enter Your Name</h1>
-      </div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Type your name..." value={inputVal} onChange={(e) => setInputVal(e.target.value)}/>
-        <button type="submit">Predict Data</button>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="p-4 shadow-md bg-white rounded-md">
+        <h1 className="text-2xl font-semibold mb-4 text-black">Enter Your Name</h1>
+      <form onSubmit={handleSubmit} className="space-y-3">
+        <input type="text" placeholder="Type your name..." value={inputVal} onChange={(e) => setInputVal(e.target.value)} className="w-full p-2 border-gray-300 rounded text-black" />
+        <button type="submit" className="w-full py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white">Predict Data</button>
       </form>
+      </div>
     </div>
   )
 }
